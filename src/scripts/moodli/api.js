a243d4d6c29804662d6c99b00d93eb7422790309
Nadav Tasher <tashernadav@gmail.com>
2020-01-22 23:55:04 +0200

    Frontend

--- src/scripts/moodli/api.js
+++ src/scripts/moodli/api.js
@@ -14,6 +14,7 @@ class Moodli {
                 UI.page("prompt");
             } else {
                 UI.page("personal");
+                Moodli.statistics();
             }
         }, Authenticate.authenticate());
     }
@@ -26,6 +27,62 @@ class Moodli {
         API.send(this.API, "report", {mood: parseInt(mood)}, (success, result) => {
             if (success) {
                 UI.page("personal");
+                Moodli.statistics();
+            } else {
+                UI.popup(result);
+            }
+        }, Authenticate.authenticate());
+    }
+
+    /**
+     * Loads the statistics.
+     */
+    static statistics() {
+        // Send a request
+        API.send(this.API, "statistics", {}, (success, result) => {
+            if (success) {
+                // Create calendar
+                let calendar = document.createElement("div");
+                // Make calendar a column
+                UI.column(calendar);
+                // Create day headings
+                let days = document.createElement("div");
+                // Make it a row
+                UI.row(days);
+                // Add day strings
+                for (let day of ["S", "M", "T", "W", "T", "F", "S"]) {
+                    // Create paragraph
+                    let paragraph = document.createElement("p");
+                    // Set text
+                    paragraph.innerText = day;
+                    // Append to days
+                    days.appendChild(paragraph);
+                }
+                // Append heading
+                calendar.appendChild(days);
+                // Loop through current year on map
+                let currentYear = new Date().getFullYear();
+                // Check if we have the current year in out map
+                if (result.hasOwnProperty(currentYear.toString())) {
+                    // Fetch year
+                    let yearMap = result[currentYear.toString()];
+                    // Get the first day-of-week of the current year
+                    let date = new Date();
+                    date.setTime(0);
+                    date.setFullYear(currentYear, 0, 1);
+                    let dayOfWeek = date.getDay();
+                    // Create rows
+                    let row;
+                    // Loop through
+                    for (let day in yearMap){
+                        // Get day of week
+
+                    }
+                }
+                // Remove all from page
+                UI.clear("personal");
+                // Add calendar to page
+                UI.get("personal").appendChild(calendar);
             } else {
                 UI.popup(result);
             }
